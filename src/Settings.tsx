8f37c8ea1046e06af77d9a7fb88772de8e68e9f8
Tarek Auf der Strasse <tarek.aufderstrasse@gmail.com>
2026-02-27 22:40:02 +0100

    feat: enhance UI components with Focusable and ScrollPanelGroup for better navigation

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,7 +1,10 @@
 import {
   ButtonItem,
+  Focusable,
+  Navigation,
   PanelSection,
   PanelSectionRow,
+  ScrollPanelGroup,
   TextField,
 } from "@decky/ui";
 import { callable, toaster } from "@decky/api";
@@ -64,56 +67,60 @@ export const SettingsPage: FC = () => {
   };
 
   return (
-    <div
+    <Focusable
+      onCancelButton={() => Navigation.NavigateBack()}
+      onCancelActionDescription="Back"
       style={{
         marginTop: "40px",
         height: "calc(100% - 40px)",
         overflow: "auto",
       }}
     >
-      {loading ? (
-        <PanelSection title="Loading...">
-          <PanelSectionRow>Loading settings...</PanelSectionRow>
-        </PanelSection>
-      ) : (
-        <>
-          <PanelSection title="RomM Connection">
-            <PanelSectionRow>
-              <TextField
-                label="RomM URL"
-                description="e.g. http://192.168.1.100:8080"
-                value={rommUrl}
-                onChange={(e) => setRommUrl(e.target.value)}
-              />
-            </PanelSectionRow>
-
-            <PanelSectionRow>
-              <TextField
-                label="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </PanelSectionRow>
-
-            <PanelSectionRow>
-              <TextField
-                label="Password"
-                bIsPassword
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </PanelSectionRow>
+      <ScrollPanelGroup>
+        {loading ? (
+          <PanelSection title="Loading...">
+            <PanelSectionRow>Loading settings...</PanelSectionRow>
           </PanelSection>
+        ) : (
+          <>
+            <PanelSection title="RomM Connection">
+              <PanelSectionRow>
+                <TextField
+                  label="RomM URL"
+                  description="e.g. http://192.168.1.100:8080"
+                  value={rommUrl}
+                  onChange={(e) => setRommUrl(e.target.value)}
+                />
+              </PanelSectionRow>
 
-          <PanelSection>
-            <PanelSectionRow>
-              <ButtonItem layout="below" onClick={onSave}>
-                Save
-              </ButtonItem>
-            </PanelSectionRow>
-          </PanelSection>
-        </>
-      )}
-    </div>
+              <PanelSectionRow>
+                <TextField
+                  label="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </PanelSectionRow>
+
+              <PanelSectionRow>
+                <TextField
+                  label="Password"
+                  bIsPassword
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </PanelSectionRow>
+            </PanelSection>
+
+            <PanelSection>
+              <PanelSectionRow>
+                <ButtonItem layout="below" onClick={onSave}>
+                  Save
+                </ButtonItem>
+              </PanelSectionRow>
+            </PanelSection>
+          </>
+        )}
+      </ScrollPanelGroup>
+    </Focusable>
   );
 };
